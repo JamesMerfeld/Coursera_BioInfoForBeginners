53db79727c564d11f173fdd5f363fd4a0f6b5754
James Merfeld <james.merfeld1@navy.mil>
2016-05-20 15:30:39 -0400

    Recovered some lost work.

--- Replication.py
+++ Replication.py
@@ -1,3 +1,12 @@
+# Input:  Strings Pattern and Text
+# Output: The number of times Pattern appears in Text
+def PatternCount(Pattern, Text):
+    count = 0 # output variable
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+                count = count+1
+    return count
+    
 # Input:  A string Text and an integer k
 # Output: A list containing all most frequent k-mers in Text
 def FrequentWords(Text, k):
