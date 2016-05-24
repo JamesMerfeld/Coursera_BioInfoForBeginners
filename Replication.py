e9b1d779f0996e7b35bc69f8a701fb7d1e6aca15
James Merfeld <james.merfeld1@navy.mil>
2016-05-24 09:06:50 -0400

    Saving work.

--- Replication.py
+++ Replication.py
@@ -85,7 +85,36 @@ def ReverseComplement(Pattern):
         else:
             print("Error:Invalid Character")
     return "".join(revComp)
+    
+def SymbolArray(Genome, symbol):
+    array = {}
+    n = len(Genome)
+    ExtendedGenome = Genome + Genome[0:n//2]
+    for i in range(n):
+        array[i] = PatternCount(symbol, ExtendedGenome[i:i+(n//2)])
+    return array
 
-Pattern = "CTTGATCAT"
-Genome = "CTTGATCATCTTGATCATCTTGATCAT"
-print(PatternMatching(Pattern, Genome))
+def FasterSymbolArray(Genome, symbol):
+    array = {}
+    n = len(Genome)
+    ExtendedGenome = Genome + Genome[0:n//2]
+    array[0] = PatternCount(symbol, Genome[0:n//2])
+    for i in range(1,n):
+        array[i] = array[i-1]
+        if ExtendedGenome[i-1] == symbol:
+            array[i] = array[i]-1
+        if ExtendedGenome[i+(n//2)-1] == symbol:
+            array[i] = array[i]+1
+    return array
+    
+def Skew(Genome):
+    skew = {}
+    n = len(Genome)
+    skew[0] = 0
+    for i in range(1,n+1):
+        skew[i] = skew[i-1]
+        if Genome[i-1] == "G":
+            skew[i] = skew[i]+1
+        elif Genome[i-1] == "C":
+            skew[i] = skew[i]-1
+    return skew
